6eea23ff71ad5207f749927ea9af31a871aca714
GTNicholson <grahamn@rtis.co.uk>
2019-10-06 20:42:53 +0100

    refrehsing of OT numbers on Sales Order form

--- CodeDev/SM_ERP/Sales/GUI/fccSalesOrderDetail.vb
+++ CodeDev/SM_ERP/Sales/GUI/fccSalesOrderDetail.vb
@@ -208,4 +208,16 @@ Public Class fccSalesOrderDetail
     Next
   End Sub
 
+
+  Public Sub RefreshWorkOrderNos(ByRef rSalesOrderItem As dmSalesOrderItem)
+    Dim mDSO As dsoSales
+    Dim mWONo As String
+    mDSO = New dsoSales(pDBConn)
+    For Each mWO As dmWorkOrder In rSalesOrderItem.WorkOrders
+      mWONo = mDSO.WorkOrderNoFromID(mWO.WorkOrderID)
+      mWO.WorkOrderNo = mWONo
+    Next
+  End Sub
+
+
 End Class
