374a143399b6c436372508142f8547033ca24bea
GTNicholson <grahamn@rtis.co.uk>
2020-01-16 06:04:10 +0000

    spread sheet export

--- CodeDev/SM_ERP/StockAdmin/Reports/clsExcelExportStockTake.vb
+++ CodeDev/SM_ERP/StockAdmin/Reports/clsExcelExportStockTake.vb
@@ -3,6 +3,17 @@
   Private pStockTake As dmStockTake
   Private pStockTakeItemEditors As colStockTakeItemEditors
 
+  Private pCurrentSheet As DevExpress.Spreadsheet.Worksheet
+
+  Private Const cColumnRowPos As Integer = 4
+
+  Private Const cColPosStockCode As Integer = 2
+
+  Public Sub New(ByRef rStockTake As dmStockTake, ByRef rStockTakeItemEditors As colStockTakeItemEditors)
+    pStockTake = rStockTake
+    pStockTakeItemEditors = rStockTakeItemEditors
+  End Sub
+
   Public Property WorkBook As DevExpress.Spreadsheet.Workbook
     Get
       Return pSpreadSheet
@@ -13,6 +24,31 @@
   End Property
 
   Public Sub CreateSpreadSheet()
+    pSpreadSheet = New DevExpress.Spreadsheet.Workbook
+    pCurrentSheet = pSpreadSheet.Worksheets(0)
+    AddHeader()
+    AddColumnHeaders()
+    AddItems()
+  End Sub
+
+  Private Sub AddHeader()
+    pCurrentSheet.Cells(1, 1).SetValue(pStockTake.Description)
+  End Sub
+
+  Private Sub AddColumnHeaders()
+    pCurrentSheet.Cells(cColumnRowPos, cColPosStockCode).SetValue("Stock Code")
 
   End Sub
+
+  Private Sub AddItems()
+    Dim mRow As Integer
+
+    mRow = cColumnRowPos + 1
+
+    For Each mItem As clsStockTakeItemEditor In pStockTakeItemEditors
+      pCurrentSheet.Cells(mRow, cColPosStockCode).SetValue(mItem.StockItem.Description)
+      mRow = mRow + 1
+    Next
+  End Sub
+
 End Class
