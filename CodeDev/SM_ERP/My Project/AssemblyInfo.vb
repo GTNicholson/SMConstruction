cadbc0afb93cbae04ca42e69eaaf2ce86853aa45
GTNicholson <grahamn@rtis.co.uk>
2020-02-02 18:14:33 +0000

    just install

--- CodeDev/SM_ERP/My Project/AssemblyInfo.vb
+++ CodeDev/SM_ERP/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' mediante el carácter '*', como se muestra a continuación:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("17.1.35.0")>
-<Assembly: AssemblyFileVersion("17.1.35.0")>
+<Assembly: AssemblyVersion("17.1.36.0")>
+<Assembly: AssemblyFileVersion("17.1.36.0")>
